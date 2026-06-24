f67c4ca8cb2b7905a70af18ff6893428f45049f0
kamala1205 <studyverse0512@gmail.com>
2026-06-25 03:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1102,5 +1102,5 @@ if __name__ == '__main__':
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
